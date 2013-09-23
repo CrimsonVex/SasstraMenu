7c2e0a3d81e50be74411abcc913a9b49722b5353
Sasstraliss <myself@sasstraliss.org>
2013-09-23 16:54:49 +0930

    Added textures and multi-level menus. Added basic auto spacing.

---
+++ Assets/Scripts/MemberInfoGetting.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Linq.Expressions;
+
+public class MemberInfoGetting : MonoBehaviour
+{
+    public static string GetMemberName<T>(Expression<Func<T>> memberExpression)
+    {
+        MemberExpression expressionBody = (MemberExpression)memberExpression.Body;
+        return expressionBody.Member.Name;
+    }
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class Menu : MonoBehaviour
 {
+    private delegate void MenuState();
+    private MenuState menuState;
+
+    GameObject currentMenuItemsSet;
     GameObject wrapperPrefab, buttonPrefab;
-    GameObject Wrapper, Button;
+
+    //string[] MenuItems = { "" };
+    string[] MainMenuItems = { "Play", "Options", "Exit" };
+    string[] OptionsMenuItems = { "Audio", "Video" };
 
 	void Awake ()
 	{
@@ -13,19 +21,57 @@ public class Menu : MonoBehaviour
 
         if (buttonPrefab != null)
             Debug.Log("SUCCESS: Retrieved button prefab!");
+
+        this.menuState = MainMenu;
+        CreateMenu(MainMenuItems);
 	}
 
-    void Start()
-    {
-        Button = Instantiate(buttonPrefab) as GameObject;
+	void Update()
+	{
+        this.menuState();
+	}
 
-        if (Button != null)
-            Debug.Log("SUCCESS: Created Button GameObject!");
+    public void MainMenu()
+    {
+        if (MenuManager.Instance.clickedObject)
+        {
+            if (MenuManager.Instance.clickedObject.name == "Options")
+            {
+                Destroy(currentMenuItemsSet);
+                this.menuState = Options;
+                CreateMenu(OptionsMenuItems);
+            }
+        }
     }
 
-	void Update ()
-	{
-        if (MenuManager.Instance.clickedObject != null)
-            Debug.Log("We clicked a button!");
-	}
+    public void Options()
+    {
+        if (MenuManager.Instance.clickedObject)
+        {
+            if (MenuManager.Instance.clickedObject.name == "Audio")
+            {
+                Destroy(currentMenuItemsSet);
+                Debug.Log("AUDIO!");
+            }
+        }
+    }
+
+    // ----------------------------------------------------------------------
+
+    void CreateMenu(string[] MenuItemsList)
+    {
+        int numButtons = MenuItemsList.Length;
+        string parentName = MemberInfoGetting.GetMemberName(() => MenuItemsList);
+        currentMenuItemsSet = new GameObject(parentName);
+        for (int i = 0; i < numButtons; i++)
+        {
+            GameObject Button = Instantiate(buttonPrefab, new Vector3(0, numButtons - i * 2, 0), Quaternion.identity) as GameObject;
+            Button.name = MenuItemsList[i];
+            Button.transform.parent = currentMenuItemsSet.transform;
+            Texture2D tex = Resources.Load("ButtonTextures/" + Button.name) as Texture2D;
+            Button.renderer.material.mainTexture = tex;
+        }
+    }
 }
+
+// Utilise: http://wiki.unity3d.com/index.php/MainMenu
